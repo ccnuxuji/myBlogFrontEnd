80915ddd38cedb8676a5fed3b9e12e6fc24f9cdb
jixu <ccnuxuji@gmail.com>
2019-10-06 16:10:51 +0800

    bug1

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -11,7 +11,7 @@ export interface AuthResponseData {
   providedIn: 'root'
 })
 export class AuthService {
-  private API = '//localhost:8080/techroad';
+  private API = '//39.108.89.204:8080/techroad';
 
   constructor(private http: HttpClient) {
   }
